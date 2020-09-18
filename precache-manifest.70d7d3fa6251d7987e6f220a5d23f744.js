9a44d40eef93cc0749c958db09aea249da859a28
paul foster <paul.foster@graffiti.net>
2020-09-18 20:10:46 +1000

    Updates

---
+++ precache-manifest.70d7d3fa6251d7987e6f220a5d23f744.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69eb0e673d2506be8ba7c300fb44fe62",
+    "url": "/public/index.html/index.html"
+  },
+  {
+    "revision": "4982c172012a97681faa",
+    "url": "/public/index.html/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6c3d0a7839db3d89d1f1",
+    "url": "/public/index.html/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "4982c172012a97681faa",
+    "url": "/public/index.html/static/js/2.cb90b32c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/public/index.html/static/js/2.cb90b32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3d0a7839db3d89d1f1",
+    "url": "/public/index.html/static/js/main.c57645a7.chunk.js"
+  },
+  {
+    "revision": "3ffed9004cf0e285dd7a",
+    "url": "/public/index.html/static/js/runtime-main.a17d9436.js"
+  }
+]);
