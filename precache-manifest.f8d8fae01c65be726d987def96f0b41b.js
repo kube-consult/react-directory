aa70a8b14ff7bd9536f27a93c51d33a226aaf5bb
paul foster <paul.foster@graffiti.net>
2020-09-18 21:29:11 +1000

    Updates

---
+++ precache-manifest.f8d8fae01c65be726d987def96f0b41b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8022b1c4c3ab51a38121e5ab550d6486",
+    "url": "/react-directory/index.html"
+  },
+  {
+    "revision": "4982c172012a97681faa",
+    "url": "/react-directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6c3d0a7839db3d89d1f1",
+    "url": "/react-directory/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "4982c172012a97681faa",
+    "url": "/react-directory/static/js/2.cb90b32c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-directory/static/js/2.cb90b32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3d0a7839db3d89d1f1",
+    "url": "/react-directory/static/js/main.c57645a7.chunk.js"
+  },
+  {
+    "revision": "b1fb731f18d2bed67232",
+    "url": "/react-directory/static/js/runtime-main.35c2026d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70083d9b85f54ea16c888b4e771d84f7.js"
+  "/react-directory/precache-manifest.f8d8fae01c65be726d987def96f0b41b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-directory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
